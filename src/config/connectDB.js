3e171f74c8e27e026e72c07f96efd9b7f88b16fd
tolahung <idoky19@gmail.com>
2024-02-24 17:04:21 +0700

    fixed db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize('bookingCare', 'root', null, {
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
 });
 

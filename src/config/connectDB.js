5151800d4d40f37442727fcfff268afd1d95ede9
DongPham1102 <butsapmau02@gmail.com>
2024-02-24 17:31:09 +0700

    refix host by Dong

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize('bookingCare', 'root', null, {
-    host: 'local host',
+    host: 'Localhost',
     dialect: 'mysql'
 });
 

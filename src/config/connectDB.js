4d85a28659827d310e8e336b9d30fb9125b39bd3
DongPham1102 <butsapmau02@gmail.com>
2024-02-24 17:23:45 +0700

    fix host by Dong

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('test_hoidanit', 'root', null, {
+const sequelize = new Sequelize('bookingCare', 'root', null, {
     host: 'localhost',
     dialect: 'mysql'
 });
